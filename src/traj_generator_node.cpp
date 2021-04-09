56693fc5c304deb2efc72c16fa18eed9090d95db
Ayham <a.n.alharbat@gmail.com>
2021-04-09 21:27:22 +0200

    fix y-position bug

--- src/traj_generator_node.cpp
+++ src/traj_generator_node.cpp
@@ -56,7 +56,7 @@ int main(int argc, char** argv) {
 
     msg.header.frame_id = "map";
     msg.pose.position.x = 0;
-    msg.pose.position.y = 1;
+    msg.pose.position.y = 0;
     msg.pose.orientation.x = 0;
     msg.pose.orientation.y = 0;
     msg.pose.orientation.z = 0;
